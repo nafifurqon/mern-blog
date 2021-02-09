5cb0b536658940046f677978f0b49904aab85e7d
nafifurqon <muhammadnafi98@gmail.com>
2021-02-09 20:31:19 +0700

    fix up homeAction

--- src/config/redux/action/homeAction.js
+++ src/config/redux/action/homeAction.js
@@ -1,15 +1,13 @@
 import Axios from 'axios'
 
-export const setDataBlog = () => {
-    return (dispatch) => {
-        Axios.get('http://localhost:4000/v1/blog/posts?page=2&perPage=2')
-        .then(result => {
-            const responseAPI = result.data;
+export const setDataBlog = () => (dispatch) => {
+    Axios.get('http://localhost:4000/v1/blog/posts?page=2&perPage=2')
+    .then(result => {
+        const responseAPI = result.data;
 
-            dispatch({type: 'UPDATE_DATA_BLOG', payload: responseAPI.data})
-        })
-        .catch(err => {
-            console.log('error: ', err)
-        })
-    }
+        dispatch({type: 'UPDATE_DATA_BLOG', payload: responseAPI.data})
+    })
+    .catch(err => {
+        console.log('error: ', err)
+    })
 }
