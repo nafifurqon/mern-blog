229dbc818f686a2aa66b901a8e19a1dfcbfa0e7e
nafifurqon <muhammadnafi98@gmail.com>
2021-02-09 10:12:47 +0700

    displayed data from API response to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,9 +7,9 @@ import Axios from 'axios'
 const Home = () => {
     const [dataBlog, setDataBlog] = useState([]);
     useEffect(() => {
-        Axios.get('http://localhost:4000/v1/blog/posts')
+        Axios.get('http://localhost:4000/v1/blog/posts?page=2&perPage=2')
         .then(result => {
-            console.log('data: ', result.data)
+            // console.log('data: ', result.data)
             const responseAPI = result.data;
 
             setDataBlog(responseAPI.data)
@@ -29,7 +29,13 @@ const Home = () => {
             <Gap height={20} />
             <div className="content-wrapper">
                 {dataBlog.map(blog => {
-                    return <BlogItem key={blog._id} />
+                    return <BlogItem 
+                    key={blog._id} 
+                    image={`http://localhost:4000/${blog.image}`} 
+                    title={blog.title} 
+                    body={blog.body}
+                    name={blog.author.name} 
+                    date={blog.createdAt} />
                 })}
             </div>
             <div className="pagination">
