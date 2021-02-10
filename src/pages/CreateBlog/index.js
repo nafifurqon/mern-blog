461c8a8f94d9e30a38d638593d17aad8cd78e776
nafifurqon <muhammadnafi98@gmail.com>
2021-02-10 09:12:04 +0700

    get value from user input

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Button, Input, Upload, TextArea, Gap, Link} from '../../components'
 import './createBlog.scss'
 import { useHistory } from 'react-router-dom'
 
 
 const CreateBlog = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
     const history = useHistory();
 
     return (
         <div className="blog-post">
             <Link title="kembali" onClick={() => history.push('/')} />
             <p className="title">Create New Blog Posts</p>
-            <Input label="Post Title" />
+            <Input label="Post Title" value={title} onChange={(e) => setTitle(e.target.value)} />
             <Upload />
-            <TextArea />
+            <TextArea value={body} onChange={(e) => setBody(e.target.value)} />
             <Gap height={20} />
             <div className="button-action">
                 <Button title="Save" />
