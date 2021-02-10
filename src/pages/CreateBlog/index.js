08df814b8742f2c308aa681aab2cf5ae2b260480
nafifurqon <muhammadnafi98@gmail.com>
2021-02-10 09:24:39 +0700

    get value from user upload image when button save clicked

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -7,18 +7,20 @@ import { useHistory } from 'react-router-dom'
 const CreateBlog = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
+    const [image, setImage] = useState('');
     const history = useHistory();
 
     const onSubmit = () => {
         console.log('title: ', title);
         console.log('body: ', body);
+        console.log('image: ', image);
     }
     return (
         <div className="blog-post">
             <Link title="kembali" onClick={() => history.push('/')} />
             <p className="title">Create New Blog Posts</p>
             <Input label="Post Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <Upload />
+            <Upload onChange={(e) => setImage(e.target.files[0])} />
             <TextArea value={body} onChange={(e) => setBody(e.target.value)} />
             <Gap height={20} />
             <div className="button-action">
